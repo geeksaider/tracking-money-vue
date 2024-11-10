b27de602b09c704d4c8ef5a235702c3e41ed72d0
geeksaider <nikita.bondarev.06@gmail.com>
2024-11-10 12:08:36 +0300

    Update Money.vue

--- components/Money.vue
+++ components/Money.vue
@@ -13,6 +13,8 @@ const historyData = ref([]);
 const categoryForPush = ref("");
 const newCategory = ref("");
 
+const typeStatic = ref("");
+
 onMounted(() => {
     if (getItem("dateProject") != null) {
         let historyDataStorage = JSON.parse(getItem("dateProject"));
@@ -29,7 +31,6 @@ onMounted(() => {
         for (let i = 0; i < categorysMemory.length; i++) {
             categorysArr.value[i] = categorysMemory[i];
         }
-        console.log(categorysArr);
     } else {
         let defaultCategorys = [
             "Все",
@@ -54,10 +55,14 @@ onMounted(() => {
     }
 });
 
-watch(categorysArr, () => {
-    setItem("categorysArr", JSON.stringify(categorysArr.value));
-    console.log(categorysArr);
-});
+watch(
+    categorysArr,
+    () => {
+        console.log("sds");
+        setItem("categorysArr", JSON.stringify(categorysArr.value));
+    },
+    { deep: true }
+);
 
 function update(e) {
     if (e.target.value == "plus" && moneys.value != "" && types.value != "") {
@@ -90,6 +95,45 @@ function update(e) {
 
     setItem("finiteResult", result.value);
     setItem("dateProject", JSON.stringify(historyData.value));
+}
+
+function static_update() {
+    historyData.value = [];
+    let staticArr = JSON.parse(getItem("dateProject"));
+    let dateStatic = new Date();
+    if (typeStatic.value == "week") {
+        for (let i = 0; i < staticArr.length; i++) {
+            staticArr[i].date = new Date(Date.parse(staticArr[i].date));
+            if (
+                staticArr[i].date.getDate() >= dateStatic.getDate() - 7 &&
+                staticArr[i].date.getMonth() == dateStatic.getMonth() &&
+                staticArr[i].date.getFullYear() == dateStatic.getFullYear()
+            ) {
+                console.log(staticArr[i]);
+                historyData.value.push(staticArr[i]);
+            }
+        }
+    } else if (typeStatic.value == "month") {
+        for (let i = 0; i < staticArr.length; i++) {
+            staticArr[i].date = new Date(Date.parse(staticArr[i].date));
+            if (
+                staticArr[i].date.getMonth() == dateStatic.getMonth() &&
+                staticArr[i].date.getFullYear() == dateStatic.getFullYear()
+            ) {
+                console.log(staticArr[i]);
+                historyData.value.push(staticArr[i]);
+            }
+        }
+    } else if (typeStatic.value == "year") {
+        dateStatic = dateStatic.getFullYear();
+        for (let i = 0; i < staticArr.length; i++) {
+            staticArr[i].date = new Date(Date.parse(staticArr[i].date));
+            if (staticArr[i].date.getFullYear() == dateStatic) {
+                console.log(staticArr[i]);
+                historyData.value.push(staticArr[i]);
+            }
+        }
+    }
 }
 
 function countExpenessAndIncome() {
@@ -166,6 +210,7 @@ function formatDate(date) {
 
 function deleteCategory(category) {
     categorysArr.value = categorysArr.value.filter((c) => c != category);
+    console.log(categorysArr.value);
 }
 
 function deleteArr(now, cost) {
@@ -179,132 +224,180 @@ function deleteArr(now, cost) {
 
 <template>
     <div
-        class="bg-white px-40 py-20 flex flex-col gap-8 min-w-max items-center justify-center rounded-2xl my-20"
+        class="bg-white px-40 py-20 flex gap-20 min-w-max justify-center rounded-2xl my-20"
     >
-        <h1 class="text-4xl font-bold">Общий баланс</h1>
-
-        <div class="flex gap-20 items-center my-6">
-            <div class="flex flex-col items-center gap-2">
-                <h1 class="text-emerald-500 font-bold text-xl">Доходы</h1>
-                <span class="text-lg font-semibold text-emerald-400">{{
-                    income
-                }}</span>
-            </div>
-            <span class="text-2xl text-stone-700 font-bold"
-                >{{ result }} ₽</span
-            >
-            <div class="flex flex-col items-center gap-2">
-                <h1 class="text-rose-500 font-bold text-xl">Расходы</h1>
-                <span class="text-lg text-rose-400 font-semibold">{{
-                    expenses
-                }}</span>
-            </div>
-        </div>
-        <div class="flex flex-col w-[500px]">
-            <div class="flex gap-5">
+        <div class="flex flex-col gap-3">
+            <h1 class="text-4xl font-bold">Статистика</h1>
+            <div class="mt-3">
                 <input
-                    type="text"
-                    placeholder="Категория"
-                    class="rounded-xl border-2 hover:outline-stone-300 text-center text-stone-700 py-1 w-full min-w-0"
-                    v-model="newCategory"
+                    type="radio"
+                    name="static"
+                    id="static_week"
+                    value="week"
+                    class="mr-3"
+                    v-model="typeStatic"
                 />
-                <button
-                    class="bg-stone-700 rounded-2xl px-4 py-1 text-white hover:scale-110 transition-all"
-                    @click="updateCategory"
-                >
-                    Добавить
-                </button>
+                <label for="static_week">Статистикак за неделю</label>
             </div>
-            <div
-                class="max-h-24 overflow-y-auto flex flex-col mt-9 w-full gap-5"
+            <div>
+                <input
+                    type="radio"
+                    name="static"
+                    value="month"
+                    id="static_month"
+                    class="mr-3"
+                    v-model="typeStatic"
+                />
+                <label for="static_month">Статистикак за месяц</label>
+            </div>
+            <div>
+                <input
+                    type="radio"
+                    name="static"
+                    value="year"
+                    id="static_year"
+                    class="mr-3"
+                    v-model="typeStatic"
+                />
+                <label for="static_year">Статистикак за год</label>
+            </div>
+            <button
+                class="bg-stone-700 rounded-xl px-4 py-1 text-white hover:scale-110 transition-all"
+                @click="static_update"
             >
-                <div
-                    v-for="category in categorysArr"
-                    class="flex justify-between"
+                Узнать
+            </button>
+        </div>
+        <div class="items-end flex flex-col">
+            <h1 class="text-4xl font-bold">Общий баланс</h1>
+
+            <div class="flex gap-20 items-center my-6">
+                <div class="flex flex-col items-center gap-2">
+                    <h1 class="text-emerald-500 font-bold text-xl">Доходы</h1>
+                    <span class="text-lg font-semibold text-emerald-400">{{
+                        income
+                    }}</span>
+                </div>
+                <span class="text-2xl text-stone-700 font-bold"
+                    >{{ result }} ₽</span
                 >
-                    <span>{{ category }}</span
-                    ><button
-                        class="mr-4 bg-stone-300 rounded-2xl px-4 py-1 text-black hover:scale-105 transition-all"
-                        @click="() => deleteCategory(category)"
-                    >
-                        Удалить
-                    </button>
+                <div class="flex flex-col items-center gap-2">
+                    <h1 class="text-rose-500 font-bold text-xl">Расходы</h1>
+                    <span class="text-lg text-rose-400 font-semibold">{{
+                        expenses
+                    }}</span>
                 </div>
             </div>
-        </div>
-        <div class="flex flex-col gap-10 items-center">
-            <div class="flex flex-row justify-between gap-8">
-                <div class="flex flex-col items-center w-[50%] gap-2">
+            <div class="flex flex-col w-[500px]">
+                <div class="flex gap-5">
                     <input
                         type="text"
+                        placeholder="Категория"
                         class="rounded-xl border-2 hover:outline-stone-300 text-center text-stone-700 py-1 w-full min-w-0"
-                        v-model="types"
+                        v-model="newCategory"
                     />
-                    <span class="text-stone-700 font-semibold"
-                        >Имя покупки</span
+                    <button
+                        class="bg-stone-700 rounded-2xl px-4 py-1 text-white hover:scale-110 transition-all"
+                        @click="updateCategory"
                     >
+                        Добавить
+                    </button>
                 </div>
-                <div class="flex flex-col items-center w-[25%] gap-2">
-                    <select
-                        name="categorys"
-                        id="categorys"
-                        class="rounded-xl border-2 hover:outline-stone-300 text-center text-stone-700 py-1 w-full min-w-0"
-                        v-model="categoryForPush"
-                    >
-                        <option
-                            v-for="category in categorysArr"
-                            :value="category"
-                        >
-                            {{ category }}
-                        </option>
-                    </select>
-                    <span class="text-stone-700 font-semibold">Категория</span>
-                </div>
-                <div class="flex flex-col items-center w-[25%] gap-2">
-                    <input
-                        type="number"
-                        min="0"
-                        step="100"
-                        class="rounded-xl border-2 hover:outline-stone-300 text-center text-stone-700 w-full py-1 min-w-0"
-                        v-model="moneys"
-                    />
-                    <span class="text-stone-700 font-semibold">Цена</span>
-                </div>
-            </div>
-            <div class="flex gap-10 mb-3">
-                <button
-                    class="bg-emerald-200 rounded-2xl px-6 py-2 text-stone-800 hover:scale-110 transition-all"
-                    value="plus"
-                    @click="update"
-                >
-                    Добавить
-                </button>
-                <button
-                    class="bg-rose-200 rounded-2xl px-7 py-2 text-stone-800 hover:scale-110 transition-all"
-                    value="minus"
-                    @click="update"
-                >
-                    Убавить
-                </button>
-            </div>
-            <div class="flex flex-col w-full gap-8">
                 <div
-                    v-for="object in historyData"
-                    class="flex justify-between gap-8 items-center w-full"
+                    class="max-h-24 overflow-y-auto flex flex-col mt-9 w-full gap-5"
                 >
-                    <p class="text-lg font-bold">{{ object.type }}</p>
                     <div
-                        class="flex justify-between gap-4 text-stone-500 items-center font-bold"
+                        v-for="category in categorysArr"
+                        class="flex justify-between"
                     >
-                        <span>{{ object.category }}</span>
-                        <span>{{ object.cost }} ₽</span>
-                        <span>{{ formatDate(object.date) }}</span>
-                        <button
-                            class="bg-rose-200 rounded-2xl px-2 py-1 text-stone-500 hover:scale-110 transition-all"
-                            @click="() => deleteArr(object.date, object.cost)"
+                        <span>{{ category }}</span
+                        ><button
+                            class="mr-4 bg-stone-300 rounded-2xl px-4 py-1 text-black hover:scale-105 transition-all"
+                            @click="() => deleteCategory(category)"
                         >
                             Удалить
                         </button>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-10 items-center">
+                <div class="flex flex-row justify-between gap-8">
+                    <div class="flex flex-col items-center w-[50%] gap-2">
+                        <input
+                            type="text"
+                            class="rounded-xl border-2 hover:outline-stone-300 text-center text-stone-700 py-1 w-full min-w-0"
+                            v-model="types"
+                        />
+                        <span class="text-stone-700 font-semibold"
+                            >Имя покупки</span
+                        >
+                    </div>
+                    <div class="flex flex-col items-center w-[25%] gap-2">
+                        <select
+                            name="categorys"
+                            id="categorys"
+                            class="rounded-xl border-2 hover:outline-stone-300 text-center text-stone-700 py-1 w-full min-w-0"
+                            v-model="categoryForPush"
+                        >
+                            <option
+                                v-for="category in categorysArr"
+                                :value="category"
+                            >
+                                {{ category }}
+                            </option>
+                        </select>
+                        <span class="text-stone-700 font-semibold"
+                            >Категория</span
+                        >
+                    </div>
+                    <div class="flex flex-col items-center w-[25%] gap-2">
+                        <input
+                            type="number"
+                            min="0"
+                            step="100"
+                            class="rounded-xl border-2 hover:outline-stone-300 text-center text-stone-700 w-full py-1 min-w-0"
+                            v-model="moneys"
+                        />
+                        <span class="text-stone-700 font-semibold">Цена</span>
+                    </div>
+                </div>
+                <div class="flex gap-10 mb-3">
+                    <button
+                        class="bg-emerald-200 rounded-2xl px-6 py-2 text-stone-800 hover:scale-110 transition-all"
+                        value="plus"
+                        @click="update"
+                    >
+                        Добавить
+                    </button>
+                    <button
+                        class="bg-rose-200 rounded-2xl px-7 py-2 text-stone-800 hover:scale-110 transition-all"
+                        value="minus"
+                        @click="update"
+                    >
+                        Убавить
+                    </button>
+                </div>
+                <div class="flex flex-col w-full gap-8">
+                    <div
+                        v-for="object in historyData"
+                        class="flex justify-between gap-8 items-center w-full"
+                    >
+                        <p class="text-lg font-bold">{{ object.type }}</p>
+                        <div
+                            class="flex justify-between gap-4 text-stone-500 items-center font-bold"
+                        >
+                            <span>{{ object.category }}</span>
+                            <span>{{ object.cost }} ₽</span>
+                            <span>{{ formatDate(object.date) }}</span>
+                            <button
+                                class="bg-rose-200 rounded-2xl px-2 py-1 text-stone-500 hover:scale-110 transition-all"
+                                @click="
+                                    () => deleteArr(object.date, object.cost)
+                                "
+                            >
+                                Удалить
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
